bdda42dba305f5da733b98d572125948c1c3acd9
Dominic Small <dominic.small@ntlworld.com>
2018-03-23 20:03:05 +0000

    Retweets now

--- cat-retweeter.py
+++ cat-retweeter.py
@@ -2,18 +2,30 @@ import tweepy
 from credentials import *
 from time import sleep
 from datetime import date, timedelta
+import wget
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 yesterday = date.today() - timedelta(1)
-print(str(yesterday))
+print(str(yesterday) + '\n')
 
 tweets = tweepy.Cursor(api.search,
                         q='#cats #catsoftwitter',
                         count='200',
                         since=str(yesterday),
                         lang='en').items()
+
 for tweet in tweets:
-    print(tweet.user.screen_name)
+    media = tweet.entities.get('media', [])
+    if len(media) > 0:
+        print("@{}:".format(tweet.user.screen_name))
+        try:
+            print(tweet.text)
+            print()
+            api.retweet(tweet.id)
+            break
+        except tweepy.TweepError as e:
+            print(e.reason)
+            print()
