66f642825a9968d01e614c1cb2c91165a7416623
Dominic Small <dominicpsmall@gmail.com>
2018-10-22 14:16:30 +0100

    Removed wget

--- cat-retweeter.py
+++ cat-retweeter.py
@@ -1,7 +1,6 @@
 import tweepy
 from credentials import *
 from datetime import date, timedelta
-import wget
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
