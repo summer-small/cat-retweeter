ff32abd37a263d64fe555066b6299115fad7a119
Dominic Small <dominicpsmall@gmail.com>
2018-11-20 13:15:53 +0000

    Begun holiday specific searches. Made initial holiday list (Christmas and Halloween). Added code to check date and update query. Updated Pipfile.

--- cat-retweeter.py
+++ cat-retweeter.py
@@ -1,22 +1,40 @@
 import tweepy
 from os import environ
-from datetime import date, timedelta
+from datetime import datetime, date, timedelta
+import pandas as pd
 
+# Setup tweepy authorization
 auth = tweepy.OAuthHandler(environ['consumer_key'], environ['consumer_secret'])
 auth.set_access_token(environ['access_token'], environ['access_token_secret'])
 api = tweepy.API(auth)
 
-yesterday = date.today() - timedelta(1)
+# Get dates
+today = date.today()
+yesterday = today - timedelta(1)
 print(str(yesterday) + '\n')
 
+# Default query
+query = '#cats #catsoftwitter'
+
+# Check for holidays
+holidays = pd.read_csv('data/holidays.csv', dtype='str')
+today_str = datetime.strftime(today, '%d/%m')
+holiday = holidays[holidays['date'] == today_str]
+
+if not holiday.empty:
+    query = query + ' ' + holiday['hashtag']
+
+# Get tweets posted since yesterday (i.e. today) for query
+# Get 200 just to be safe
 tweets = tweepy.Cursor(
     api.search,
-    q='#cats #catsoftwitter',
+    q=query,
     count='200',
     since=str(yesterday),
     lang='en'
 ).items()
 
+# Retweet first appropriate tweet
 for tweet in tweets:
     media = tweet.entities.get('media', [])
     if len(media) > 0:
