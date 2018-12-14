0b9cf90e74ed1188ea70ee44fdbf581f164286de
Dominic Small <dominicpsmall@gmail.com>
2018-12-14 17:06:14 +0000

    Added more holidays. Now uses holiday hastag value to construct query.

--- cat-retweeter.py
+++ cat-retweeter.py
@@ -6,12 +6,14 @@ import pandas as pd
 # Setup tweepy authorization
 auth = tweepy.OAuthHandler(environ['consumer_key'], environ['consumer_secret'])
 auth.set_access_token(environ['access_token'], environ['access_token_secret'])
+#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # Get dates
 today = date.today()
 yesterday = today - timedelta(1)
-print(str(yesterday) + '\n')
+print("Yesterday:", str(yesterday))
 
 # Default query
 query = '#cats #catsoftwitter'
@@ -21,8 +23,12 @@ holidays = pd.read_csv('data/holidays.csv', dtype='str')
 today_str = datetime.strftime(today, '%d/%m')
 holiday = holidays[holidays['date'] == today_str]
 
+# Append holiday hashtag to query if today is a holiday
 if not holiday.empty:
-    query = query + ' ' + holiday['hashtag']
+    query =  query + ' ' + holiday['hashtag'].values[0]
+    print("Holiday:", holiday['event'].values[0])
+
+print("Query:", query)
 
 # Get tweets posted since yesterday (i.e. today) for query
 # Get 200 just to be safe
@@ -34,7 +40,7 @@ tweets = tweepy.Cursor(
     lang='en'
 ).items()
 
-# Retweet first appropriate tweet
+# Retweet first tweet with attached media
 for tweet in tweets:
     media = tweet.entities.get('media', [])
     if len(media) > 0:
