0e8b5de5a6de8408faaeabd4a957f847d8f2c86e
Dominic Small <dominicpsmall@gmail.com>
2019-05-08 21:50:21 +0100

    Added media filter at query instead of removing non-medai tweets after search. Also added no-retweets filter.

--- cat-retweeter.py
+++ cat-retweeter.py
@@ -7,13 +7,13 @@ import pandas as pd
 # Add keys and tokens to credentials.py (already in gitignore)
 # Uncomment lines below and comment out standard auth lines to use
 # Alternatively, set keys and tokens as environment variables
-#from credentials import *
-#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-#auth.set_access_token(access_token, access_token_secret)
+from credentials import *
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
 # Setup tweepy authorization
-auth = tweepy.OAuthHandler(environ['consumer_key'], environ['consumer_secret'])
-auth.set_access_token(environ['access_token'], environ['access_token_secret'])
+#auth = tweepy.OAuthHandler(environ['consumer_key'], environ['consumer_secret'])
+#auth.set_access_token(environ['access_token'], environ['access_token_secret'])
 api = tweepy.API(auth)
 
 # Get date
@@ -33,7 +33,13 @@ if not holiday.empty:
     query =  query + ' ' + holiday['hashtag'].values[0]
     print("Holiday:", holiday['event'].values[0])
 
+# Add filters:
+# - Must have media
+# - Not a retweet
+query += " filter:media -filter:retweets"
+
 print("Query:", query)
+print("-"*48)
 
 # Loop until cat media tweet found
 cat_found = False
@@ -44,7 +50,7 @@ while not cat_found:
     tweets = tweepy.Cursor(
         api.search,
         q=query,
-        count='200',
+        count='100',
         since=str(today),
         lang='en'
     ).items()
@@ -52,19 +58,18 @@ while not cat_found:
     # Retweet first tweet with attached media
     # that has not already been retweeted
     for tweet in tweets:
-        media = tweet.entities.get('media', [])
-        if len(media) > 0:
-            print("@{}".format(tweet.user.screen_name))
-            print(tweet.created_at)
-            print(tweet.text)
-            try:
-                api.retweet(tweet.id)
-                cat_found = True
-                break
-            except tweepy.TweepError as e:
-                print(e.reason, "\n")
+        print("@{}".format(tweet.user.screen_name))
+        print(tweet.created_at)
+        print(tweet.text)
+        print("-"*48)
+        try:
+            #api.retweet(tweet.id)
+            cat_found = True
+            break
+        except tweepy.TweepError as e:
+            print(e.reason, "\n")
     
     # If there are no holiday cat media tweets, change to default query
     if not cat_found:
-        query = "#cats #catsoftwitter"
+        query = "#cats #catsoftwitter filter:media -filter:retweets"
         print("New query:", query)
